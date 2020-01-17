b9a5238fb7de8fb8853ad012748e18fae5df061a
Кретов Евгений Михайлович <kretov.em@yandex.ru>
2020-01-17 11:49:58 +0300

    Занятие 1. Базовый настройки MVC

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -24,6 +24,8 @@ namespace WebStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Добавляем сервисы, необоходимые для mvc
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,16 +38,19 @@ namespace WebStore
 
             var helloMessage = _configuration["CustomHelloWorld"];
             var logLevel = _configuration["Logging:LogLevel:Microsoft"];
-
-
+            
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync(helloMessage);
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync(helloMessage);
+                //});
             });
         }
     }
