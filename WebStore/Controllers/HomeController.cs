f06ff4c02463d64ac5d11c6e20618c2f9476edc1
Кретов Евгений Михайлович <kretov.em@yandex.ru>
2021-11-11 15:56:14 +0300

    Занятие 4: Рефакторинг

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -45,13 +45,5 @@ namespace WebStore.Controllers
         {
             return View();
         }
-        public IActionResult ProductDetails()
-        {
-            return View();
-        }
-        public IActionResult Shop()
-        {
-            return View();
-        }
     }
 }
