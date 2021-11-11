8d31cf1f5a283bf04ee378619bdbcd63e0d72bbb
Кретов Евгений Михайлович <kretov.em@yandex.ru>
2021-11-11 16:06:35 +0300

    Занятие 4: добавленны доменные модели и InMemoryProductService

---
+++ WebStore/Infrastructure/Interface/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using WebStore.Domain.Entities.Base;
+
+namespace WebStore.Infrastructure.Interface
+{
+    public interface IProductService
+    {
+        IEnumerable<Category> GetCategories();
+        IEnumerable<Brand> GetBrands();
+    }
+}
